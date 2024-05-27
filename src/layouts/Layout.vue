8786a764f9be2f294ce5d40285cf5e885a9c667d
pmateos805 <pmateos805@gmail.com>
2024-05-27 10:33:04 +0000

    feat [f]: intro with dark mode

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="bg-gradient-to-tr from-rose-100 to-sky-100 dark:bg-gradient-to-tr dark:from-sky-950 dark:via-gray-800">
+    <nav class="flex justify-end items-center p-4 fixed w-full">
+      <ToggleTheme />
+    </nav>
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+import ToggleTheme from "../components/ToggleTheme.vue";
+</script>
